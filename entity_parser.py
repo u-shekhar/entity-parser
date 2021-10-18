36f9c3e3c0ce946536f651c8781227534b8761a8
u-shekhar <ujjwal0101@gmail.com>
2021-10-18 14:30:16 +0530

    updated

--- entity_parser.py
+++ entity_parser.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flasgger import Swagger
 import pickle
 import spacy
@@ -10,13 +10,40 @@ warnings.filterwarnings('ignore')
 app = Flask(__name__)
 Swagger(app)
 
+model_path = r"pehla_pehla_model.pkl"
+pickled_model = open(model_path,"rb")
+parser = pickle.load(pickled_model)
+
 @app.route('/')
 def welcome():
-    return "Running"
+    return "Runing! add /apidocs at the url end to go to parser"
+
+@app.route('/parse', methods=['POST'])
+def get_entities():
+    """ Entity Parser for Grocery Recommender
+    ---
+    parameters:
+     - name: input_string
+       in: query
+       type: string
+       required: true
+    responses:
+       200:
+            description: Entities are 
+
+    """
+
+    input_string = request.args.get("input_string")
+    doc = parser(input_string)
+    op = ""
+    for ent in doc.ents :
+        op = op + ent.text + ":" + ent.label_ + ";"
+
+    return op
 
 if __name__ == "__main__":
     # Use debug only when required. With debug no need to stop the server every time.
-    #app.run()
-    port = int(os.environ.get('PORT',5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+    #port = int(os.environ.get('PORT',5000))
+    #app.run(host='0.0.0.0', port=port)
     # Use Cntl+C to stop the server.
