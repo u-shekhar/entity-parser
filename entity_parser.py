feb37851e04fedb0c423d46679f394598df887f0
u-shekhar <ujjwal0101@gmail.com>
2021-10-18 14:19:52 +0530

    basic

--- entity_parser.py
+++ entity_parser.py
@@ -1,13 +1,10 @@
+from flask import Flask
+from flasgger import Swagger
+import pickle
 import spacy
 import os
-from flask import Flask, render_template, request
-from flasgger import Swagger
-import requests
-import pickle
-
-model_path = r"pehla_pehla_model.pkl"
-pickled_model = open(model_path,"rb")
-parser = pickle.load(pickled_model)
+import warnings
+warnings.filterwarnings('ignore')
 
 # Defining the name (will fetch from the actual file name)
 app = Flask(__name__)
@@ -16,30 +13,6 @@ Swagger(app)
 @app.route('/')
 def welcome():
     return "Running"
-# Defingin the path or url attahced after the local ip
-@app.route('/parse', methods=['POST'])
-def get_entities():
-    """ Entity Parser for Grocery Recommender
-    ---
-    parameters:
-     - name: input_string
-       in: query
-       type: string
-       required: true
-    responses:
-       200:
-            description: Result is
-
-    """
-
-    input = request.args.get("input_string")
-    doc = parser(input)
-    op = ""
-    for ent in doc.ents :
-        op = op + ent.text + ":" + ent.label_ + ";"
-
-    return op
-
 
 if __name__ == "__main__":
     # Use debug only when required. With debug no need to stop the server every time.
